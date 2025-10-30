68db3be50b6c1408748097372e0a39fe1aca26b3
sydas <96981480+Tegardas@users.noreply.github.com>
2025-10-30 14:37:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -375,7 +375,7 @@ class EWSDashboard {
             };
 
             // Connect using WebSocket
-            const url = `wss://${MQTT_CONFIG.broker}:${MQTT_CONFIG.port}`;
+            const url = `wss://${MQTT_CONFIG.broker}:${MQTT_CONFIG.port}/mqtt`;
             this.mqttClient = mqtt.connect(url, options);
             // this.mqttClient = new WebSocket(url);
 
